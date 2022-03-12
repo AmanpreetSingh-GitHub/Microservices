a612f1966c71c8258ef69d41f86dd9d4bac4f327
Amanpreet Singh <amanpreet.singh@hotmail.com>
2022-03-13 01:19:15 +0530

    Shopping.Aggregator implementation

---
+++ src/ApiGateways/Shopping.Aggregator/Controllers/ShoppingController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.Aggregator.Models;
+using Shopping.Aggregator.Services;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ShoppingController : ControllerBase
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
+        private readonly IOrderService _orderService;
+
+        public ShoppingController(ICatalogService catalogService, IBasketService basketService, IOrderService orderService)
+        {
+            _catalogService = catalogService ?? throw new ArgumentNullException(nameof(catalogService));
+            _basketService = basketService ?? throw new ArgumentNullException(nameof(basketService));
+            _orderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
+        }
+
+        [HttpGet("{userName}", Name = "GetShopping")]
+        [ProducesResponseType(typeof(ShoppingModel), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingModel>> GetShopping(string userName)
+        {
+            // -------------------------
+            // Get Basket with username
+            // Iterate basket items and consume products with basket item productId member
+            // Map product related members into basketitem dto with extended columns
+            // consume ordering microservices in order to retrieve order list
+            // return root ShoppingModel dto class which including all responses
+            // -------------------------
+
+            // Get Basket with username
+            var basket = await _basketService.GetBasket(userName);
+
+            // Iterate basket items and consume products with basket item productId member
+            foreach(var item in basket.Items)
+            {
+                var product = await _catalogService.GetCatalogById(item.ProductId);
+
+                // Map product related members into basketitem dto with extended columns
+                item.ProductName = product.Name;
+                item.Category = product.Category;
+                item.Summary = product.Summary;
+                item.Description = product.Description;
+                item.ImageFile = product.ImageFile;
+            }
+
+            // consume ordering microservices in order to retrieve order list
+            var orders = await _orderService.GetOrdersByUserName(userName);
+
+            // return root ShoppingModel dto class which including all responses
+            var shoppingModel = new ShoppingModel
+            {
+                Username = userName,
+                BasketWithProducts = basket,
+                Orders = orders
+            };
+
+            return Ok(shoppingModel);
+        }
+    }
+}
