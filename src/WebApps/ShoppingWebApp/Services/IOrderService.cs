c6f8ba235081dd88ece16d9e5115952709147c2b
Amanpreet Singh <amanpreet.singh@hotmail.com>
2022-03-14 01:12:30 +0530

    ShoppingWebApp implemented

---
+++ src/WebApps/ShoppingWebApp/Services/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using ShoppingWebApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ShoppingWebApp.Services
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+    }
+}
